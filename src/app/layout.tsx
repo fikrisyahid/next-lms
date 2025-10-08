82c3b85e6fdca5ddbc843b3a7f325ea0c49cb6ee
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2025-10-08 22:57:15 +0700

    fix: disable nextjs default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import "@mantine/core/styles.css";
@@ -14,15 +13,6 @@ import { Notifications } from "@mantine/notifications";
 import { ModalsProvider } from "@mantine/modals";
 import ProgressProviders from "@/components/progress-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -40,7 +30,6 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <MantineProvider>
           <Notifications />
