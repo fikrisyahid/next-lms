b43b509c01f1ee26ad3e878c5731bd9ea5bc04db
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2025-10-11 23:51:30 +0700

    fix: delete user notification color

--- src/app/(admin)/admin/users/components/delete-user-modal.tsx
+++ src/app/(admin)/admin/users/components/delete-user-modal.tsx
@@ -35,6 +35,7 @@ export default function DeleteUserModal({
       notifications.show({
         title: "Success",
         message: "User deleted successfully",
+        color: "green",
       });
     } catch {
       notifications.show({
