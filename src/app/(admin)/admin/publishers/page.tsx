86c04cb1c81cfe9e8567fe87ab177284df2fd56d
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2025-10-21 03:32:25 +0700

    refactor: skeleton for publisher table

--- src/app/(admin)/admin/publishers/page.tsx
+++ src/app/(admin)/admin/publishers/page.tsx
@@ -1,22 +1,18 @@
-import { Title } from "@mantine/core";
+import { Skeleton, Title } from "@mantine/core";
 import AddPublisherModal from "./components/add-publisher-modal";
-import { getAllPublishers } from "@/app/api/publishers";
 import PublisherTable from "./components/table";
+import { Suspense } from "react";
 
 export default async function PublishersPage() {
-  const publishers = await getAllPublishers();
-
-  if (publishers.status === "error") {
-    return <div>Error: {publishers.message}</div>;
-  }
-
   return (
     <div className="flex flex-col gap-4 items-stretch">
       <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
         <Title>Daftar Penerbit</Title>
         <AddPublisherModal />
       </div>
-      <PublisherTable />
+      <Suspense fallback={<Skeleton height={80} radius="md" />}>
+        <PublisherTable />
+      </Suspense>
     </div>
   );
 }
